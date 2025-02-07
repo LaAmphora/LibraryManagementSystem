92731d193444de3cfdb7951ca54abfd52db630cd
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-02-07 13:39:35 -0600

    Added all members of the Book class. Created a default constructor, parameterized constructor, and a copy constructor for the Book class. Kenzie and I worked on this step together. Still need JavaDoc comments on the constructors.

--- src/edu/mu/manage/Book.java
+++ src/edu/mu/manage/Book.java
@@ -1,5 +1,31 @@
 package edu.mu.manage;
 
 public class Book {
-
+	
+	private String title;
+	private String author;
+	private String ISBN;
+	private double price;
+	
+	
+	public Book() {
+		this.title = "Unknown";
+		this.author = "Unknown";
+		ISBN = "Unknown";
+		this.price = 0.0;
+	}
+	
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+	
+	public Book(Book copy)
+	{
+		this(copy.title, copy.author, copy.ISBN, copy.price);
+	}
+	
+	
 }
