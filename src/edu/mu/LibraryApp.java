522a56d1f7be5c3737adbbdb97d687d0c79ea7f3
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-02-08 15:19:15 -0600

    Javadoc Comments for the Book Class
    Finished the Javadoc comments for the following functions:
    - default constructor for Book object
    - parameterized constructor for Book object
    - copy constructor for Book object

--- src/edu/mu/LibraryApp.java
+++ src/edu/mu/LibraryApp.java
@@ -1,11 +1,15 @@
 package edu.mu;
 
+import edu.mu.manage.Book;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//kenzie was here
-
+		Book book;
+		book = new 
 	}
+	
 
 }
--- src/edu/mu/manage/Book.java
+++ src/edu/mu/manage/Book.java
@@ -8,6 +8,10 @@ public class Book {
 	private double price;
 	
 	
+	/**
+	 * This is a default constructor, that will allow
+	 * you to create a generic Book object.
+	 * */
 	public Book() {
 		this.title = "Unknown";
 		this.author = "Unknown";
@@ -15,6 +19,18 @@ public class Book {
 		this.price = 0.0;
 	}
 	
+	/**
+	 * This a parameterized constructor, that will allow you
+	 * to create a Book object with a title, author, ISBN, and price.
+	 * @param title (String)
+	 * 		This argument is the the title of the book.
+	 * @param author (String)
+	 * 		This argument is the author of the book.
+	 * @param ISBN (String)
+	 * 		This argument is the ISBN of the book.
+	 * @param price (double)
+	 * 		This argument is the price of the book.
+	 * */
 	public Book(String title, String author, String ISBN, double price) {
 		this.title = title;
 		this.author = author;
@@ -22,8 +38,13 @@ public class Book {
 		this.price = price;
 	}
 	
-	public Book(Book copy)
-	{
+	/**
+	 * This is a copy constructor, that will allow you to copy
+	 * the members from a Book object to another book object.
+	 * @param copy (Book)
+	 * 		This argument is the Book object the user wants to copy.
+	 * */
+	public Book(Book copy) {
 		this(copy.title, copy.author, copy.ISBN, copy.price);
 	}
 	
