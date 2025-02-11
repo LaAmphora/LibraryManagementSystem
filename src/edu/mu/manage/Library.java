daedbc33b722ba96f245394d655d942fc042ff55
kenziemcallister <mackenziemcallister64@gmail.com>
2025-02-10 19:11:33 -0600

    Created addBook method and Getters and Setters
    - Created default constructor to initialize book array.
    - The addBook function adds a book to the books array.
      returns true if there is space, false if not.
    - Created getters and setters for books and count.
    - Included JavaDocs for each.

--- src/edu/mu/manage/Library.java
+++ src/edu/mu/manage/Library.java
@@ -5,4 +5,74 @@ public class Library {
 	private Book[] books;
 	private int count;
 	
+	/**
+	 * This is a default constructor that initializes
+	 * the Book array with a fixed size of 5,
+	 * and the count to zero.
+	 */
+	public Library() {
+		this.books = new Book[5]; //creating a fixed size of 5
+		this.count = 0;
+	}
+	
+	/**
+	 * This is a method used to add a book to the books array.
+	 * If there is no more room in the array, the method
+	 * displays an error message. Otherwise, the book is
+	 * added and the count is incremented.
+	 * 
+	 * @param book
+	 * 		This is the array of Books that is to be added to.
+	 * @return
+	 * 		Returns true if there is enough space in the array
+	 * 		to add a book. Returns false if there is not enough
+	 * 		space within the array.s
+	 */
+	public boolean addBook(Book book) {
+		
+		if (count < getBooks().length) {
+			books[count] = book;
+			count++;
+			return true;
+		} else {
+			System.out.println("The library is full. You must remove a book before adding a new one.");
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * Getter for Book[] array.
+	 * @return
+	 * 		Returns the books array.
+	 */
+	public Book[] getBooks() {
+		return books;
+	}
+
+	/**
+	 * Setter for Book[] array.
+	 * @param books
+	 */
+	public void setBooks(Book[] books) {
+		this.books = books;
+	}
+
+	/**
+	 * Getter for count variable.
+	 * @return
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/**
+	 * Setter for count variable.
+	 * @param count
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	
 }
