fa1bdd5bbd62edcf0d68714848118f949156605f
kenziemcallister <mackenziemcallister64@gmail.com>
2025-02-10 20:04:52 -0600

    Test class progress
    - Tested previously made methods and constructors using the LibraryApp
    class that acts as a main.
    - Added 3 books to a library, searched for a book by ISBN, and displayed
    the results.

--- src/edu/mu/LibraryApp.java
+++ src/edu/mu/LibraryApp.java
@@ -1,10 +1,29 @@
 package edu.mu;
 
+import edu.mu.manage.Library;
+import edu.mu.manage.Book;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Kenzie was here
+		
+		Library kenziesLibrary = new Library(); //creating a library object
+		
+		Book siestaLane = new Book("Siesta Lane", "Amy Minato", "12345", 10.0);
+		Book theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "54321", 15.0);
+		Book mathDestruction = new Book("Weapons of Math Destruction", "Cathy O'Neil", "32154", 20.0);
+		
+		//adding at least three books to the library
+		kenziesLibrary.addBook(siestaLane);
+		kenziesLibrary.addBook(theHobbit);
+		kenziesLibrary.addBook(mathDestruction);
+		
+		//printing the library
+		kenziesLibrary.displayBooks();
+		
+		//searching for book by ISBN, displays book object's information as result
+		System.out.println("Found book with 54321 as ISBN: "+ kenziesLibrary.searchByISBN("54321").toString());
+		
 	}
 	
 
